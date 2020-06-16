e2b93ccc9c70bf7c339148272f3d7e0d86863174
Emil Raj Schmidt <emil4746@elevcampus.dk>
2020-06-16 08:23:03 +0200

    Færdig med første dag på game design

---
+++ GameDesignProject/Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody2D Rigidbody {get; set;}
+
+    [SerializeField]
+    private float movementSpeed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 position = transform.position;
+        position.x += movementSpeed * Time.deltaTime * Input.GetAxis("Horizontal");
+        position.y += movementSpeed * Time.deltaTime * Input.GetAxis("Vertical");
+        transform.position = position;
+    }
+}
